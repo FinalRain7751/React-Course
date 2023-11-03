4a7ea3e804cbafc05918ad565be8d76d9323d7fd
Souransu Sen <souransu2sen@gmail.com>
2023-11-03 11:25:24 +0530

    complete

---
+++ src/components/Modal.jsx
@@ -0,0 +1,28 @@
+import { createPortal } from "react-dom";
+
+import { motion } from "framer-motion";
+
+export default function Modal({ title, children, onClose }) {
+  const hiddenAnimationState = { opacity: 0, y: 30 };
+  return createPortal(
+    <>
+      <div className="backdrop" onClick={onClose} />
+      <motion.dialog
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        open
+        className="modal"
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        transition={{ duration: 0.5 }}
+      >
+        <h2>{title}</h2>
+        {children}
+      </motion.dialog>
+    </>,
+    document.getElementById("modal")
+  );
+}
